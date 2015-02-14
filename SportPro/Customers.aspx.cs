3c9853405831a4a07c3825d1f5b81f3a5484d02e
mr-moshi <Froggish90@gmail.com>
2015-02-14 16:04:13 -0500

    Worked on the code-behind of Customers (non-func)

--- SportPro/Customers.aspx.cs
+++ SportPro/Customers.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class Customers : System.Web.UI.Page
 {
@@ -13,6 +14,16 @@ public partial class Customers : System.Web.UI.Page
     }
     protected void ddlCustomers_SelectedIndexChanged(object sender, EventArgs e)
     {
-        DataBind customer = (DataBind)sqlSelected.Select(DataSourceSelectArguments.Empty);
+        DataView dvSQL = (DataView)sqlDDLCustomer.Select(DataSourceSelectArguments.Empty);
+        Customer selectedCustomer = new Customer();
+        foreach (DataRowView drvSQL in dvSQL)
+        {
+            if (drvSQL["CustomerID"].ToString() == ddlCustomers.SelectedValue)
+                selectedCustomer = new Customer(drvSQL["Name"].ToString(), drvSQL["Address"].ToString(), 
+                    drvSQL["City"].ToString(), drvSQL["Zipcode"].ToString(), drvSQL["Phone"].ToString(), 
+                    drvSQL["Email"].ToString(), (Char)drvSQL["State"], (int)drvSQL["CustomerID"]);
+        }
+
+        lblAddress1.Text = selectedCustomer.Address;
     }
 }
