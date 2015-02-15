fd28b490baea1c1be4cd3a8d8f47c2e0aa3f00db
ScottLegrove <scottlegrove@gmail.com>
2015-02-15 18:50:04 -0500

    changes to CustomerList class

---
+++ SportPro/App_Code/CustomerList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.HttpContext;
+
+/// <summary>
+/// Summary description for ContactList
+/// </summary>
+public class CustomerList
+{
+    private List<Customer> Customer;
+    public int Count;
+
+    public CustomerList()
+    {
+        this.Customer = new List<Customer>();
+    }
+ 
+    public Customer this[int index]
+    {
+        get
+        {
+            return Customer[index];
+        }
+
+        set
+        {
+            Customer[index] = value;
+        }
+    }
+
+    public Customer this[string name]
+    {
+        get
+        {
+            int indexOf = this.Customer.FindIndex(f => f.Name == name);
+            return Customer[indexOf];
+        }
+
+        set
+        {
+            int indexOf = this.Customer.FindIndex(f => f.Name == name);
+            Customer[indexOf] = value;
+        }
+    }
+
+    public static CustomerList GetCustomers()
+    {
+        CustomerList session = (CustomerList)HttpContext.Current.Session["customerList"];
+        
+        if (HttpContext.Current.Session["customerList"] == null)
+        {
+            session = new CustomerList();
+            HttpContext.Current.Session["customerList"] = session;
+        }
+       
+        return session;  
+    }
+    public void AddItem(Customer customer)
+    {
+
+    }
+    public void RemoveAt(int index)
+    {
+
+    }
+    public void Clear()
+    {
+
+    }
+
+
+
+}
